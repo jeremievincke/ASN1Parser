1705f63447f41deaf201d6b6f78211998dc20396
Jérémie Vincke <jeremie@movify.be>
2016-11-15 12:53:18 +0100

    Project initialization - Used DTFoundation sources

---
+++ ASN1Parser/ASN1Parser/ASN1Parser.h
@@ -0,0 +1,20 @@
+//
+//  ASN1Parser.h
+//  ASN1Parser
+//
+//  Created by Jérémie Vincke on 15/11/2016.
+//  Copyright © 2016 JV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASN1Parser.
+FOUNDATION_EXPORT double ASN1ParserVersionNumber;
+
+//! Project version string for ASN1Parser.
+FOUNDATION_EXPORT const unsigned char ASN1ParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASN1Parser/PublicHeader.h>
+
+#import <ASN1Parser/DTASN1Parser.h>
+
---
+++ ASN1Parser/ASN1ParserTests/ASN1ParserTests.m
@@ -0,0 +1,39 @@
+//
+//  ASN1ParserTests.m
+//  ASN1ParserTests
+//
+//  Created by Jérémie Vincke on 15/11/2016.
+//  Copyright © 2016 JV. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ASN1ParserTests : XCTestCase
+
+@end
+
+@implementation ASN1ParserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
